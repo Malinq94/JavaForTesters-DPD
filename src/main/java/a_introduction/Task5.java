2c75d9c1f2e71f896d66b5aa51caa549b23e557e
Malinq94 <d.malinowski1@gmail.com>
2023-04-20 18:51:32 +0200

    Hello world 1st

--- src/main/java/a_introduction/Task5.java
+++ src/main/java/a_introduction/Task5.java
@@ -2,6 +2,13 @@ package a_introduction;
 
 public class Task5 {
     public static void main(String[] args) {
-
+    String imie = "Damian";
+    int wiek = 17;
+    if (wiek<=18) {
+        System.out.println(imie + " ma " + wiek + " lat i jest niepełnoletni");
+    }
+    else {
+        System.out.println(imie + " ma " + wiek + " lat i jest pełnoletni");
+    }
     }
 }
--- src/main/java/a_introduction/Task6.java
+++ src/main/java/a_introduction/Task6.java
@@ -2,6 +2,10 @@ package a_introduction;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        double alcoholContent = 5.5;
+        final String msg = alcoholContent >= 4
+                ? "Mocne"
+                : "Słabe" ;
+        System.out.println(msg);
     }
 }
